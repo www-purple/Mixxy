c717fbf57a47314bd99f5b7572e26f95a1e14a46
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-03-10 06:18:09 -0500

    Move everything to the www.purple.mixxy package (and subpackages)

---
+++ mixxy/src/main/java/www/purple/mixxy/models/ArticlesDto.java
@@ -0,0 +1,11 @@
+package www.purple.mixxy.models;
+
+import java.util.List;
+
+import www.purple.mixxy.models.Article;
+
+public class ArticlesDto {
+
+    public List<Article> articles;
+
+}
---
+++ mixxy/src/test/java/www/purple/mixxy/controllers/NinjaAppengineBackendTest.java
@@ -0,0 +1,34 @@
+package www.purple.mixxy.controllers;
+
+import org.junit.After;
+import org.junit.Before;
+
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.googlecode.objectify.ObjectifyService;
+
+import www.purple.mixxy.conf.ObjectifyProvider;
+
+import java.io.Closeable;
+
+public class NinjaAppengineBackendTest {
+
+    private final LocalServiceTestHelper helper = new LocalServiceTestHelper(
+            new LocalDatastoreServiceTestConfig());
+    
+    Closeable session;
+
+    @Before
+    public void setUp() {
+        helper.setUp();
+        session = ObjectifyService.begin();
+        ObjectifyProvider.setup();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        helper.tearDown();
+        session.close();
+    }
+
+}
