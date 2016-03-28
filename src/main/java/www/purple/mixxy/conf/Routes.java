60587e117bc9d81cf826b7ad9629c6a41428ced7
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-03-27 22:56:39 -0400

    Reformat Routes.java

--- src/main/java/www/purple/mixxy/conf/Routes.java
+++ src/main/java/www/purple/mixxy/conf/Routes.java
@@ -29,63 +29,64 @@ import www.purple.mixxy.controllers.LoginLogoutController;
 
 @SuppressWarnings("PMD.LawOfDemeter")
 public class Routes implements ApplicationRoutes {
-    
-    @Inject
-    private NinjaProperties ninjaProperties;
 
-    /**
-     * Using a (almost) nice DSL we can configure the router.
-     * 
-     * The second argument NinjaModuleDemoRouter contains all routes of a
-     * submodule. By simply injecting it we activate the routes.
-     * 
-     * @param router
-     *            The default router of this application
-     */
-    @Override
-    public void init(final Router router) {  
-        
-        // puts test data into db:
-        if (!ninjaProperties.isProd()) {
-            router.GET().route("/setup").with(ApplicationController.class, "setup");
-        }
-        
-        ///////////////////////////////////////////////////////////////////////
-        // Login / Logout
-        ///////////////////////////////////////////////////////////////////////
-        router.GET().route("/login").with(LoginLogoutController.class, "login");
-        router.POST().route("/login").with(LoginLogoutController.class, "loginPost");
-        router.GET().route("/logout").with(LoginLogoutController.class, "logout");
-        
-        ///////////////////////////////////////////////////////////////////////
-        // Create new article
-        ///////////////////////////////////////////////////////////////////////
-        router.GET().route("/article/new").with(ArticleController.class, "articleNew");
-        router.POST().route("/article/new").with(ArticleController.class, "articleNewPost");
-        
-        ///////////////////////////////////////////////////////////////////////
-        // Create new article
-        ///////////////////////////////////////////////////////////////////////
-        router.GET().route("/article/{id}").with(ArticleController.class, "articleShow");
+  @Inject
+  private NinjaProperties ninjaProperties;
 
-        ///////////////////////////////////////////////////////////////////////
-        // Api for management of software
-        ///////////////////////////////////////////////////////////////////////
-        router.GET().route("/api/{username}/articles.json").with(ApiController.class, "getArticlesJson");
-        router.GET().route("/api/{username}/articles.xml").with(ApiController.class, "getArticlesXml");
-        router.POST().route("/api/{username}/article.json").with(ApiController.class, "postArticleJson");
-        router.POST().route("/api/{username}/article.xml").with(ApiController.class, "postArticleXml");
- 
-        ///////////////////////////////////////////////////////////////////////
-        // Assets (pictures / javascript)
-        ///////////////////////////////////////////////////////////////////////    
-        router.GET().route("/assets/webjars/{fileName: .*}").with(AssetsController.class, "serveWebJars");
-        router.GET().route("/assets/{fileName: .*}").with(AssetsController.class, "serveStatic");
-        
-        ///////////////////////////////////////////////////////////////////////
-        // Index / Catchall shows index page
-        ///////////////////////////////////////////////////////////////////////
-        router.GET().route("/.*").with(ApplicationController.class, "index");
+  /**
+   * Using a (almost) nice DSL we can configure the router.
+   * 
+   * The second argument NinjaModuleDemoRouter contains all routes of a
+   * submodule. By simply injecting it we activate the routes.
+   * 
+   * @param router
+   *          The default router of this application
+   */
+  @Override
+  public void init(final Router router) {
+
+    // puts test data into db:
+    if (!ninjaProperties.isProd()) {
+      router.GET().route("/setup").with(ApplicationController.class, "setup");
     }
+
+
+    ///////////////////////////////////////////////////////////////////////
+    // Login / Logout
+    ///////////////////////////////////////////////////////////////////////
+    router.GET().route("/login").with(LoginLogoutController.class, "login");
+    router.POST().route("/login").with(LoginLogoutController.class, "loginPost");
+    router.GET().route("/logout").with(LoginLogoutController.class, "logout");
+
+    ///////////////////////////////////////////////////////////////////////
+    // Create new article
+    ///////////////////////////////////////////////////////////////////////
+    router.GET().route("/article/new").with(ArticleController.class, "articleNew");
+    router.POST().route("/article/new").with(ArticleController.class, "articleNewPost");
+
+    ///////////////////////////////////////////////////////////////////////
+    // Create new article
+    ///////////////////////////////////////////////////////////////////////
+    router.GET().route("/article/{id}").with(ArticleController.class, "articleShow");
+
+    ///////////////////////////////////////////////////////////////////////
+    // Api for management of software
+    ///////////////////////////////////////////////////////////////////////
+    router.GET().route("/api/{username}/articles.json").with(ApiController.class, "getArticlesJson");
+    router.GET().route("/api/{username}/articles.xml").with(ApiController.class, "getArticlesXml");
+    router.POST().route("/api/{username}/article.json").with(ApiController.class, "postArticleJson");
+    router.POST().route("/api/{username}/article.xml").with(ApiController.class, "postArticleXml");
+
+    ///////////////////////////////////////////////////////////////////////
+    // Assets (pictures / javascript)
+    ///////////////////////////////////////////////////////////////////////
+    router.GET().route("/assets/webjars/{fileName: .*}").with(AssetsController.class, "serveWebJars");
+    router.GET().route("/assets/{fileName: .*}").with(AssetsController.class, "serveStatic");
+
+    ///////////////////////////////////////////////////////////////////////
+    // Index / Catchall shows index page
+    ///////////////////////////////////////////////////////////////////////
+    router.GET().route("/.*").with(ApplicationController.class, "index");
+  }
 
 }
