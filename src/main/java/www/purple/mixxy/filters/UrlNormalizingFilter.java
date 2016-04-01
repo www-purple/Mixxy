53acb74b6b94e1b65e708d3a935358e13b10b9fa
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-04-01 01:06:33 -0400

    Improve UrlNormalizingFilter
    - Add possibility of multiple transformations
    - Document it

--- src/main/java/www/purple/mixxy/filters/UrlNormalizingFilter.java
+++ src/main/java/www/purple/mixxy/filters/UrlNormalizingFilter.java
@@ -6,18 +6,40 @@ import ninja.FilterChain;
 import ninja.Result;
 import ninja.Results;
 
+/**
+ * Any URLs that almost follow a specific convention, but not quite, will be
+ * fixed and the user redirected to the proper URL. This is for SEO purposes;
+ * {@code /resource} and {@code /resource/} (note the trailing slash) are two
+ * separate Web pages, as far as Google is concerned.
+ * 
+ * This {@code Filter} transforms URLs in the following ways:
+ * <ul>
+ * <li>Strips trailing slashes</li>
+ * </ul>
+ * 
+ * @author Jesse Talavera-Greenberg
+ *
+ */
 public class UrlNormalizingFilter implements Filter {
 
   @Override
   public Result filter(FilterChain filterChain, Context context) {
 
     String path = context.getRequestPath();
+    boolean transformed = false;
 
     if (path.endsWith("/")) {
-      return Results.redirect(path.substring(0, path.length() - 1));
+      transformed = true;
+      path = path.substring(0, path.length() - 1);
     }
-    
-    return filterChain.next(context);
+
+    if (transformed) {
+
+      return Results.redirect(path);
+    }
+
+    return transformed ? Results.redirect(path) : filterChain.next(context);
+    // TODO: Context might be lost!
   }
 
 }
