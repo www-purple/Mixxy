0534cd36571d33e5b19eaa14c275799ee5f3a46b
Brian Sabz <Bsabz0512@gmail.com>
2016-04-05 13:08:29 -0400

    Added the Comic Kind, modified User Kind
    - modified .gitignore for .idea specific files
    - Added enum Roles for the User Kind

--- src/main/java/www/purple/mixxy/models/User.java
+++ src/main/java/www/purple/mixxy/models/User.java
@@ -1,8 +1,13 @@
 package www.purple.mixxy.models;
 
+import com.google.appengine.api.oauth.OAuthService;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 import com.googlecode.objectify.annotation.Index;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Entity
 @Index
@@ -12,15 +17,41 @@ public class User {
     public Long id;
     public String username;
     public String password;
-    public String fullname;
-    public boolean isAdmin;
+    public String firstname;
+    public String lastname;
+    public String displayname;
+    public String email;
+    public Map<OAuthService, String> authentications = new HashMap<>();
+
+    public Role role;
+    public Date createdAt;
+    public Date updatedAt;
+    public Boolean isActive;
 
     public User() { /* Needed by Objectify */ }
     
-    public User(final String username, final String password, final String fullname) {
+    public User(final String username,
+                final String password,
+                final String firstname,
+                final String lastname,
+                final String displayname,
+                final String email) {
+
         this.username = username;
         this.password = password;
-        this.fullname = fullname;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.displayname = displayname;
+        this.email = email;
+
+        // @Chris - TODO: how are we going to use this map?
+        //this.authentications =
+
+        this.role = Role.USER;
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
+        this.isActive = true;
+
     }
  
 }
