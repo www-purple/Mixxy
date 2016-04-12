815f58dc56286203bf1202f6891d5bbac8fa939f
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-04-12 12:00:01 -0400

    Add stubs for various Dao methods

--- src/main/java/www/purple/mixxy/dao/UserDao.java
+++ src/main/java/www/purple/mixxy/dao/UserDao.java
@@ -8,23 +8,63 @@ import www.purple.mixxy.models.User;
 
 public class UserDao {
 
-    @Inject
-    private Provider<Objectify> objectify;
-    
-    public boolean isUserValid(String username) {
-        
-        if (username != null) {
+  @Inject
+  private Provider<Objectify> objectify;
 
-            User user = objectify.get().load().type(User.class)
-                    .filter("username", username).first().now();
-            
-            if (user != null) {
-                return true;              
-            }
-        }
-        
-        return false;
- 
+  /**
+   * @param username
+   *          The name of the user whose existence is to be queried.
+   * @return True if a user with this name actually exists.
+   * @TODO Can be optimized, maybe with a projection query or something.
+   */
+  public boolean isUserValid(String username) {
+
+    if (username != null) {
+
+      User user = objectify.get().load().type(User.class)
+          .filter(User.USERNAME, username).first().now();
+
+      if (user != null) {
+        // If a user with this name actually exists...
+        return true;
+      }
     }
 
+    return false;
+  }
+
+  /**
+   * Given a username, get the underlying {@link User}.
+   * 
+   * @param username
+   *          The user whose details should be retrieved.
+   * @return The {@link User} if it exists, {@code null} if doesn't or if
+   *         {@code username} is {@code null}.
+   */
+  public User getUser(String username) {
+    User user = null;
+
+    if (username != null) {
+      // If we were given a valid username...
+      user = objectify.get().load().type(User.class)
+          .filter(User.USERNAME, username).first().now();
+    }
+
+    return user;
+  }
+
+  /**
+   * Given a user ID, get the underlying {@link User}.
+   * 
+   * @param id
+   *          The ID user whose details should be retrieved.
+   * @return The {@link User} if it exists, {@code null} if doesn't.
+   */
+  public User getUser(long id) {
+    return objectify.get().load().type(User.class).id(id).now();
+  }
+  
+  public boolean updateUser(String username, User info) {
+    return false;
+  }
 }
