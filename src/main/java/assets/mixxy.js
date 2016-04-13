20c081ac1f1f07f3bde5d93e2b38dda4783c8cc2
Kathleen Cleary <Kathleen.Cleary@stonybrook.edu>
2016-04-13 18:18:12 -0400

    Fixed all issues, except hardcoded comics/data

--- src/main/java/assets/mixxy.js
+++ src/main/java/assets/mixxy.js
@@ -2,7 +2,7 @@
  * Created by MURDERBUS on 4/7/16.
  */
 $(document).ready(function(){
-    $('.featured-comics').slick({
+    $('#featured-comics').slick({
         //adaptiveHeight: true,
     });
 });
