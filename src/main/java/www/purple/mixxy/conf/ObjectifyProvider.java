ba593549019a92cceabc1190f6d3d9270d3cff3a
Brian Sabz <Bsabz0512@gmail.com>
2016-04-13 17:28:09 -0400

    Added Objectify registers for Entity Kinds

--- src/main/java/www/purple/mixxy/conf/ObjectifyProvider.java
+++ src/main/java/www/purple/mixxy/conf/ObjectifyProvider.java
@@ -5,6 +5,11 @@ import com.googlecode.objectify.Objectify;
 import com.googlecode.objectify.ObjectifyService;
 
 import www.purple.mixxy.models.Article;
+import www.purple.mixxy.models.Ban;
+import www.purple.mixxy.models.Comic;
+import www.purple.mixxy.models.Flag;
+import www.purple.mixxy.models.Like;
+import www.purple.mixxy.models.Subscription;
 import www.purple.mixxy.models.User;
 
 public class ObjectifyProvider implements Provider<Objectify> {
@@ -37,6 +42,11 @@ public class ObjectifyProvider implements Provider<Objectify> {
 
         ObjectifyService.register(User.class);
         ObjectifyService.register(Article.class);
+        ObjectifyService.register(Comic.class);
+        ObjectifyService.register(Ban.class);
+        ObjectifyService.register(Flag.class);
+        ObjectifyService.register(Like.class);
+        ObjectifyService.register(Subscription.class);
 
         setup();
     }
