aa8314ba6d061ef15f8616f00350236da2780dad
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-04-14 19:30:17 -0400

    Bind APIKeys for injection

--- src/main/java/www/purple/mixxy/conf/Module.java
+++ src/main/java/www/purple/mixxy/conf/Module.java
@@ -1,20 +1,22 @@
 package www.purple.mixxy.conf;
 
 import ninja.appengine.AppEngineModule;
-import www.purple.mixxy.helpers.FacebookAuthHelper;
-import www.purple.mixxy.helpers.FacebookGraph;
-import www.purple.mixxy.helpers.GoogleAuthHelper;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
 import com.googlecode.objectify.Objectify;
+
+import www.purple.mixxy.helpers.ApiKeys;
+import www.purple.mixxy.helpers.FacebookAuthHelper;
+import www.purple.mixxy.helpers.FacebookGraph;
+import www.purple.mixxy.helpers.GoogleAuthHelper;
 
 @Singleton
 public class Module extends AbstractModule {
     
     @Override
     protected void configure() {
-        
+        bind(ApiKeys.class);
         bind(StartupActions.class);
         bind(GoogleAuthHelper.class);
         bind(FacebookAuthHelper.class);
