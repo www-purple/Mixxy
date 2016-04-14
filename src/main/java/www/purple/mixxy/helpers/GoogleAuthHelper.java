1d8f27531e82bbe6888c9837ae06ab6b34799212
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-04-14 19:30:47 -0400

    Refactor FacebookAuthHelper and GoogleAuthHelper

--- src/main/java/www/purple/mixxy/helpers/GoogleAuthHelper.java
+++ src/main/java/www/purple/mixxy/helpers/GoogleAuthHelper.java
@@ -19,87 +19,86 @@ import java.util.Collection;
 
 public final class GoogleAuthHelper {
 
-	/**
-	 * Please provide a value for the CLIENT_ID constant before proceeding, set this up at https://code.google.com/apis/console/
-	 */
-	private static final String CLIENT_ID = "734052615495-2751ophn3s30pvldar5j7tkicgp7ga2s.apps.googleusercontent.com";
-	/**
-	 * Please provide a value for the CLIENT_SECRET constant before proceeding, set this up at https://code.google.com/apis/console/
-	 */
-	private static final String CLIENT_SECRET = "SXQxxtOPo3gMxtuW_bcaHI_O";
+  /**
+   * Callback URI that google will redirect to after successful authentication
+   */
+  private static final String CALLBACK_URI = "http://localhost:8080/validate";
 
-	/**
-	 * Callback URI that google will redirect to after successful authentication
-	 */
-	private static final String CALLBACK_URI = "http://localhost:8080/validate";
-	
-	// start google authentication constants
-	private static final Collection<String> SCOPE =
-	    Arrays.asList("https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email");
-	private static final String USER_INFO_URL = "https://www.googleapis.com/oauth2/v1/userinfo";
-	private static final JsonFactory JSON_FACTORY = new JacksonFactory();
-	private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
-	// end google authentication constants
-	
-	private String stateToken;
-	
-	private final GoogleAuthorizationCodeFlow flow;
-	
-	/**
-	 * Constructor initializes the Google Authorization Code Flow with CLIENT ID, SECRET, and SCOPE 
-	 */
-	public GoogleAuthHelper() {
-		flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT,
-				JSON_FACTORY, CLIENT_ID, CLIENT_SECRET, SCOPE).build();
-		
-		generateStateToken();
-	}
+  // start google authentication constants
+  private static final Collection<String> SCOPE = Arrays.asList("https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email");
+  private static final String USER_INFO_URL = "https://www.googleapis.com/oauth2/v1/userinfo";
+  private static final JsonFactory JSON_FACTORY = new JacksonFactory();
+  private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+  // end google authentication constants
 
-	/**
-	 * Builds a login URL based on client ID, secret, callback URI, and scope 
-	 */
-	public String buildLoginUrl() {
-		
-		final GoogleAuthorizationCodeRequestUrl url = flow.newAuthorizationUrl();
-		
-		return url.setRedirectUri(CALLBACK_URI).setState(stateToken).build();
-	}
-	
-	/**
-	 * Generates a secure state token 
-	 */
-	private void generateStateToken(){
-		
-		SecureRandom sr1 = new SecureRandom();
-		
-		stateToken = "google;"+sr1.nextInt();
-		
-	}
-	
-	/**
-	 * Accessor for state token
-	 */
-	public String getStateToken(){
-		return stateToken;
-	}
-	
-	/**
-	 * Expects an Authentication Code, and makes an authenticated request for the user's profile information
-	 * @return JSON formatted user profile information
-	 * @param authCode authentication code provided by google
-	 */
-	public String getUserInfoJson(final String authCode) throws IOException {
+  private String stateToken;
+  private String id;
+  private String secret;
 
-		final GoogleTokenResponse response = flow.newTokenRequest(authCode).setRedirectUri(CALLBACK_URI).execute();
-		final Credential credential = flow.createAndStoreCredential(response, null);
-		final HttpRequestFactory requestFactory = HTTP_TRANSPORT.createRequestFactory(credential);
-		// Make an authenticated request
-		final GenericUrl url = new GenericUrl(USER_INFO_URL);
-		final HttpRequest request = requestFactory.buildGetRequest(url);
-		request.getHeaders().setContentType("application/json");
-		final String jsonIdentity = request.execute().parseAsString();
+  private final GoogleAuthorizationCodeFlow flow;
 
-		return jsonIdentity;
+  /**
+   * Constructor initializes the Google Authorization Code Flow with CLIENT ID,
+   * SECRET, and SCOPE
+   */
+  public GoogleAuthHelper(String id, String secret) {
+    this.id = id;
+    this.secret = secret;
+    flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY, id, secret, SCOPE).build();
 
-	}
+    generateStateToken();
+  }
+
+  /**
+   * Builds a login URL based on client ID, secret, callback URI, and scope
+   */
+  public String buildLoginUrl() {
+
+    final GoogleAuthorizationCodeRequestUrl url = flow.newAuthorizationUrl();
+
+    return url.setRedirectUri(CALLBACK_URI).setState(stateToken).build();
+  }
+
+  /**
+   * Generates a secure state token
+   */
+  private void generateStateToken() {
+
+    SecureRandom sr1 = new SecureRandom();
+    // TODO: Make this secure
+
+    stateToken = "google;" + sr1.nextInt();
+
+  }
+
+  /**
+   * Accessor for state token
+   */
+  public String getStateToken() {
+    return stateToken;
+  }
+
+  /**
+   * Expects an Authentication Code, and makes an authenticated request for the
+   * user's profile information
+   * 
+   * @return JSON formatted user profile information
+   * @param authCode
+   *          authentication code provided by google
+   */
+  public String getUserInfoJson(final String authCode) throws IOException {
+
+    final GoogleTokenResponse response = flow.newTokenRequest(authCode).setRedirectUri(CALLBACK_URI).execute();
+    final Credential credential = flow.createAndStoreCredential(response, null);
+    final HttpRequestFactory requestFactory = HTTP_TRANSPORT.createRequestFactory(credential);
+    // Make an authenticated request
+    final GenericUrl url = new GenericUrl(USER_INFO_URL);
+    final HttpRequest request = requestFactory.buildGetRequest(url);
+    request.getHeaders().setContentType("application/json");
+    final String jsonIdentity = request.execute().parseAsString();
+
+    return jsonIdentity;
+
+  }
 }
