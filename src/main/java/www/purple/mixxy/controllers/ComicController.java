abeaf2dd681eceff7323066fffc0ed03d51908dc
Brian Sabz <Bsabz0512@gmail.com>
2016-04-25 13:55:15 -0400

    Added create and show method for comics

--- src/main/java/www/purple/mixxy/controllers/ComicController.java
+++ src/main/java/www/purple/mixxy/controllers/ComicController.java
@@ -1,13 +1,19 @@
 package www.purple.mixxy.controllers;
 
+import ninja.Context;
 import ninja.FilterWith;
 import ninja.Result;
 import ninja.Results;
 import ninja.appengine.AppEngineFilter;
-import www.purple.mixxy.dao.ArticleDao;
+import ninja.params.PathParam;
+import ninja.validation.JSR303Validation;
+import ninja.validation.Validation;
 import www.purple.mixxy.dao.ComicDao;
+import www.purple.mixxy.etc.LoggedInUser;
 import www.purple.mixxy.filters.JsonEndpoint;
 import www.purple.mixxy.filters.UrlNormalizingFilter;
+import www.purple.mixxy.models.Comic;
+import www.purple.mixxy.models.ComicDto;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -17,48 +23,85 @@ import com.google.inject.Singleton;
 public class ComicController {
 
 	@Inject
-    private ComicDao comicDao;
-	
-  public Result remix() {
-    return Results.TODO();
-  }
+	private ComicDao comicDao;
 
-  public Result edit() {
-    return Results.TODO();
-  }
+	@SuppressWarnings("PMD.DataflowAnomalyAnalysis")
+	public Result remix() {
+		return Results.TODO();
+	}
 
-  @FilterWith(JsonEndpoint.class)
-  public Result newWork() {
-    return Results.TODO();
-  }
+	public Result edit() {
+		return Results.TODO();
+	}
 
-  @FilterWith(JsonEndpoint.class)
-  public Result likes() {
-    return Results.TODO();
-  }
+	public Result comicShow(@PathParam("id") Long id) {
 
-  @FilterWith(JsonEndpoint.class)
-  public Result remixes() {
-    return Results.TODO();
-  }
+		Comic comic = null;
 
-  @FilterWith(JsonEndpoint.class)
-  public Result root() {
-    return Results.TODO();
-  }
+		if (id != null) {
 
-  @FilterWith(JsonEndpoint.class)
-  public Result parent() {
-    return Results.TODO();
-  }
+			comic = comicDao.getComic(id);
 
-  @FilterWith(JsonEndpoint.class)
-  public Result delete() {
-    return Results.TODO();
-  }
+		}
 
-  @FilterWith(JsonEndpoint.class)
-  public Result update() {
-    return Results.TODO();
-  }
+		return Results.html().render("comic", comic);
+
+	}
+
+	@FilterWith(JsonEndpoint.class)
+	public Result newWork(@LoggedInUser String username, Context context, @JSR303Validation ComicDto comicDto,
+			Validation validation) {
+
+		if (validation.hasViolations()) {
+
+			context.getFlashScope().error("Please correct field.");
+			context.getFlashScope().put("title", comicDto.title);
+			context.getFlashScope().put("description", comicDto.description);
+			context.getFlashScope().put("images", comicDto.images);
+			context.getFlashScope().put("likes", comicDto.likes);
+			context.getFlashScope().put("tags", comicDto.tags);
+
+			return Results.redirect("/create/");
+
+		} else {
+
+			comicDao.newComic(username, comicDto);
+
+			context.getFlashScope().success("New article created.");
+
+			return Results.redirect("/");
+
+		}
+
+	}
+
+	@FilterWith(JsonEndpoint.class)
+	public Result likes() {
+		return Results.TODO();
+	}
+
+	@FilterWith(JsonEndpoint.class)
+	public Result remixes() {
+		return Results.TODO();
+	}
+
+	@FilterWith(JsonEndpoint.class)
+	public Result root() {
+		return Results.TODO();
+	}
+
+	@FilterWith(JsonEndpoint.class)
+	public Result parent() {
+		return Results.TODO();
+	}
+
+	@FilterWith(JsonEndpoint.class)
+	public Result delete() {
+		return Results.TODO();
+	}
+
+	@FilterWith(JsonEndpoint.class)
+	public Result update() {
+		return Results.TODO();
+	}
 }
