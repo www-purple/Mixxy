55de1a100a4dad8cf8b00e273a9506380747396a
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-04-28 00:57:06 -0400

    Remove all old Article stuff
    - It's not relevant to us
    - If you need to reference it, you can look at the commit logs
    - Also remove most (but not all!) of ApiController

--- src/main/java/www/purple/mixxy/conf/ObjectifyProvider.java
+++ src/main/java/www/purple/mixxy/conf/ObjectifyProvider.java
@@ -4,7 +4,6 @@ import com.google.inject.Provider;
 import com.googlecode.objectify.Objectify;
 import com.googlecode.objectify.ObjectifyService;
 
-import www.purple.mixxy.models.Article;
 import www.purple.mixxy.models.Ban;
 import www.purple.mixxy.models.Comic;
 import www.purple.mixxy.models.Flag;
@@ -14,25 +13,6 @@ import www.purple.mixxy.models.User;
 
 public class ObjectifyProvider implements Provider<Objectify> {
 
-    public static final String POST1_TITLE = "Hello to the blog example!";
-    public static final String POST1_CONTENT
-            = "<p>Hi and welcome to the demo of Ninja!</p> "
-            + "<p>This example shows how you can use Ninja in the wild. Some things you can learn:</p>"
-            + "<ul>"
-            + "<li>How to use the templating system (header, footer)</li>"
-            + "<li>How to test your application with ease.</li>"
-            + "<li>Setting up authentication (login / logout)</li>"
-            + "<li>Internationalization (i18n)</li>"
-            + "<li>Static assets / using webjars</li>"
-            + "<li>Persisting data</li>"
-            + "<li>Beautiful <a href=\"/article/3\">html routes</a> for your application</li>"
-            + "<li>How to design your restful Api (<a href=\"/api/bob@gmail.com/articles.json\">Json</a> and <a href=\"/api/bob@gmail.com/articles.xml\">Xml</a>)</li>"
-            + "<li>... and much much more.</li>"
-            + "</ul>"
-            + "<p>We are always happy to see you on our mailing list! "
-            + "Check out <a href=\"http://www.ninjaframework.org\">our website for more</a>.</p>";
-    private static final String LIPSUM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit sed nisl sed lorem commodo elementum in a leo. Aliquam erat volutpat. Nulla libero odio, consectetur eget rutrum ac, varius vitae orci. Suspendisse facilisis tempus elit, facilisis ultricies massa condimentum in. Aenean id felis libero. Quisque nisl eros, accumsan eget ornare id, pharetra eget felis. Aenean purus erat, egestas nec scelerisque non, eleifend id ligula. eget ornare id, pharetra eget felis. Aenean purus erat, egestas nec scelerisque non, eleifend id ligula. eget ornare id, pharetra eget felis. Aenean purus erat, egestas nec scelerisque non, eleifend id ligula. eget ornare id, pharetra eget felis. Aenean purus erat, egestas nec scelerisque non, eleifend id ligula. eget ornare id, pharetra eget felis. Aenean purus erat, egestas nec scelerisque non, eleifend id ligula. eget ornare id, pharetra eget felis. Aenean purus erat, egestas nec scelerisque non, eleifend id ligula. eget ornare id, pharetra eget felis. Aenean purus erat, egestas nec scelerisque non, eleifend id ligula. eget ornare id, pharetra eget felis. Aenean purus erat, egestas nec scelerisque non, eleifend id ligula.";
-    
     @Override
     public Objectify get() {
         return ObjectifyService.ofy();
@@ -41,7 +21,6 @@ public class ObjectifyProvider implements Provider<Objectify> {
     static {
 
         ObjectifyService.register(User.class);
-        ObjectifyService.register(Article.class);
         ObjectifyService.register(Comic.class);
         ObjectifyService.register(Ban.class);
         ObjectifyService.register(Flag.class);
@@ -62,19 +41,6 @@ public class ObjectifyProvider implements Provider<Objectify> {
             User bob = new User("BobTheBuilder", "Bob", "Smith", "male", "bob@gmail.com", "url", "en", "123", "google");
             ofy.save().entity(bob).now();
 
-            // Create a new post
-            Article bobPost3 = new Article(bob, "My third post", LIPSUM);
-            ofy.save().entity(bobPost3).now();
-
-            // Create a new post
-            Article bobPost2 = new Article(bob, "My second post", LIPSUM);
-            ofy.save().entity(bobPost2).now();
-
-            // Create a new post
-            Article bobPost1 = new Article(bob, POST1_TITLE, POST1_CONTENT);
-            ofy.save().entity(bobPost1).now();
-            
-            
             // Comic ancestry working
             Comic bobComic = new Comic(null, bob, "cool title", "interesting description", null);
             ofy.save().entities(bobComic).now();
