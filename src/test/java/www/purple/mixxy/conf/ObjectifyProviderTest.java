668b864654826881a15d1f75bf7fb10164f940fc
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-05-16 21:41:40 -0400

    Add ObjectifyProviderTest

---
+++ src/test/java/www/purple/mixxy/conf/ObjectifyProviderTest.java
@@ -0,0 +1,34 @@
+package www.purple.mixxy.conf;
+
+import com.google.inject.Injector;
+import com.google.inject.Singleton;
+
+import ninja.NinjaTest;
+import www.purple.mixxy.controllers.NinjaAppengineBackendTest;
+import www.purple.mixxy.helpers.ApiKeys;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ObjectifyProviderTest extends NinjaAppengineBackendTest {
+
+  private ObjectifyProvider provider;
+
+  @Before
+  public void injectObjectifyProvider() {
+    Injector injector = this.getInjector();
+    provider = injector.getInstance(ObjectifyProvider.class);
+  }
+
+  @Test
+  public void testObjectifyProviderInjected() {
+    assertNotNull(provider);
+  }
+
+  @Test
+  public void testProviderReturnsObjectifyInstance() {
+    assertNotNull(provider.get());
+  }
+}
