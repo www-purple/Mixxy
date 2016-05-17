956bc7a1ac811e0660a19bb48483ab9250f04d72
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-05-17 16:32:46 -0400

    Add content to LayoutTest

--- src/test/java/www/purple/mixxy/views/LayoutTest.java
+++ src/test/java/www/purple/mixxy/views/LayoutTest.java
@@ -1,5 +1,23 @@
 package www.purple.mixxy.views;
 
-public class HomepageTest {
+import org.junit.Test;
 
+import ninja.NinjaFluentLeniumTest;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+import static org.junit.Assert.*;
+
+import org.fluentlenium.core.domain.FluentList;
+import org.fluentlenium.core.domain.FluentWebElement;
+
+public class LayoutTest extends NinjaFluentLeniumTest {
+
+  @Test
+  public void testAuthor() {
+    goTo(getServerAddress() + "/");
+
+    FluentList<FluentWebElement> author = find("meta", withName("author"));
+
+    assertFalse(author.isEmpty());
+    assertEquals("www.purple", author.getAttribute("content"));
+  }
 }
